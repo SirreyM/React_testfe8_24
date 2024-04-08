9689fce9f1abcf4aa901abb35c097d15056f85a8
Administrator <Administrator@192.168.3.135>
2024-04-08 14:06:15 +0000

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://apprunner.code-wizard.in:8304/testb8april"; //Temporarily using field; Enter Backend URL here e.g. http://3.133.64.67:8164/generated_app
+export const PATH_PETSERVICE = "/PetServices";
+export const PATH_PETOWNER = "/PetOwners";
+export const PATH_PETCARECENTER = "/PetCareCenters";
+export const PATH_DOCUMENT = "/Documents";
+export const PATH_PET = "/Pets";
---
+++ src/utils/display_configuration.js
@@ -0,0 +1,34 @@
+export const petCareCenterViewConfig = {
+Details: [
+    {
+      key: "Name",
+      value: "Name",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Address",
+      value: "Address",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "BranchName",
+      value: "BranchName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Pincode",
+      value: "Pincode",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
